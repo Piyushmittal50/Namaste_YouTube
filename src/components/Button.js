11cc5d53fb519ac9d841191030d49b996627eafc
piyushmittal50 <mittalpiyush87483@gmail.com>
2024-03-02 14:18:39 +0530

    .

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({name}) => {
+  return (
+    <div>
+          <Button className="bg-black text-white">{name}</Button>
+    </div>
+  )
+}
+
+export default Button;
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Button from './Button';
 
 const ButtonList = () => {
   return (
-    <div>ButtonList</div>
-  )
+    <div>
+      <Button name="All" />
+      <Button name="Music" />
+      <Button name="Movies" />
+      <Button name="News" />
+    </div>
+  );
 }
 
 export default ButtonList;
